5f10273e32f5b94d0b5427fae261e57360e7f74f
_Ma_nu_ <developeremanuele@gmail.com>
2019-09-15 08:40:03 +0200

    Initial commit

---
+++ src/com/github/devmanu/automaticbooks/AutomaticBooks.java
@@ -0,0 +1,16 @@
+package com.github.devmanu.automaticbooks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutomaticBooks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+
+
+
+
+}
